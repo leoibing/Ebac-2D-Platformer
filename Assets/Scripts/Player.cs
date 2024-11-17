9049356137a6ef9ff79e20ad6e3f25e51a196174
Leonardo Ibing <leoibing@hotmail.com>
2024-11-17 16:36:15 -0300

    Aula 2: Adicionando pulo ao personagem

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	public Rigidbody2D rb;
-	public Vector2 velocity;
+	public Vector2 friction = new Vector2(.1f, 0);
 	public float speed;
+	public float forceJump = 2;
 
 	private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+		HandleJump();
+		HandleMoviment();
+	}
+
+	private void HandleMoviment()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			//rb.MovePosition(rb.position - velocity * Time.deltaTime);
 			rb.velocity = new Vector2(-speed, rb.velocity.y);
 		}
-		else if(Input.GetKey(KeyCode.RightArrow))
+		else if (Input.GetKey(KeyCode.RightArrow))
 		{
 			//rb.MovePosition(rb.position + velocity * Time.deltaTime);
 			rb.velocity = new Vector2(speed, rb.velocity.y);
 		}
 
+		if(rb.velocity.x > 0)
+		{
+			rb.velocity -= friction;
+		}
+		else if (rb.velocity.x < 0)
+		{
+			rb.velocity += friction;
+		}
+	}
+
+	private void HandleJump()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+			rb.velocity = Vector2.up * forceJump;
 	}
 }
