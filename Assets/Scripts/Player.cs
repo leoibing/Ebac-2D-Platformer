b09accee384893ea81b54a1da1ca7ded67ad65f1
Leonardo Ibing <leoibing@hotmail.com>
2024-11-17 16:47:45 -0300

    Aula 3: Fazendo o personagem correr

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
 	public Rigidbody2D rb;
 	public Vector2 friction = new Vector2(.1f, 0);
 	public float speed;
+	public float speedRun;
 	public float forceJump = 2;
+	private float _currentSpeed;
+	//private bool _isRunning;
 
 	private void Update()
     {
@@ -17,18 +20,25 @@ public class Player : MonoBehaviour
 
 	private void HandleMoviment()
 	{
+		if(Input.GetKey(KeyCode.LeftControl))
+			_currentSpeed = speedRun;
+		else
+			_currentSpeed = speed;
+
+		//_isRunning = Input.GetKey(KeyCode.LeftControl);
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			//rb.MovePosition(rb.position - velocity * Time.deltaTime);
-			rb.velocity = new Vector2(-speed, rb.velocity.y);
+			rb.velocity = new Vector2(-_currentSpeed, rb.velocity.y);
 		}
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
 			//rb.MovePosition(rb.position + velocity * Time.deltaTime);
-			rb.velocity = new Vector2(speed, rb.velocity.y);
+			rb.velocity = new Vector2(_currentSpeed, rb.velocity.y);
+			//rb.velocity = new Vector2(Input.GetKey(KeyCode.LeftControl) ? speed : speedRun, rb.velocity.y);
 		}
 
-		if(rb.velocity.x > 0)
+		if (rb.velocity.x > 0)
 		{
 			rb.velocity -= friction;
 		}
