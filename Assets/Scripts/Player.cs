433e20280e1a84d6924c115a33cf1337590f7c9b
Leonardo Ibing <leoibing@hotmail.com>
2024-11-20 15:30:05 -0300

    Aula 5: Integrar as animações no nosso jogo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,13 +14,28 @@ public class Player : MonoBehaviour
 	public float forceJump = 2;
 
 	[Header("Animation setup")]
-	public float jumpScaleY = 1.5f;
+	public float jumpScaleY = 1.4f;
 	public float jumpScaleX = .7f;
 	public float animationDuration = .3f;
 	public Ease ease = Ease.OutBack;
 
+	[Header("Animation player")]
+	public string boolRun = "Run";
+	public string boolJump = "Jump";
+	public Animator animator;
+	public float playerSwipeDuration = .1f;
+
 	private float _currentSpeed;
 	//private bool _isRunning;
+
+	private void OnValidate()
+	{
+		if (rb == null)
+			rb = GetComponent<Rigidbody2D>();
+
+		if (animator == null)
+			animator = GetComponent<Animator>();
+	}
 
 	private void Update()
     {
@@ -30,22 +45,43 @@ public class Player : MonoBehaviour
 
 	private void HandleMoviment()
 	{
-		if(Input.GetKey(KeyCode.LeftControl))
+		if (Input.GetKey(KeyCode.LeftControl))
+		{
 			_currentSpeed = speedRun;
+			animator.speed = 2;
+		}
 		else
+		{
 			_currentSpeed = speed;
+			animator.speed = 1;
+		}
 
 		//_isRunning = Input.GetKey(KeyCode.LeftControl);
 
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			rb.velocity = new Vector2(-_currentSpeed, rb.velocity.y);
+			if(rb.transform.localScale.x != -1)
+			{
+				rb.transform.DOScaleX(-1, playerSwipeDuration);
+			}
+			animator.SetBool(boolRun, true);
 		}
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
 			//rb.MovePosition(rb.position + velocity * Time.deltaTime);
 			rb.velocity = new Vector2(_currentSpeed, rb.velocity.y);
+			if (rb.transform.localScale.x != 1)
+			{
+				rb.transform.DOScaleX(1, playerSwipeDuration);
+			}
+			//rb.transform.localScale = new Vector2(1, 1);
+			animator.SetBool(boolRun, true);
 			//rb.velocity = new Vector2(Input.GetKey(KeyCode.LeftControl) ? speed : speedRun, rb.velocity.y);
+		}
+		else
+		{
+			animator.SetBool(boolRun, false);
 		}
 
 		if (rb.velocity.x > 0)
@@ -64,9 +100,14 @@ public class Player : MonoBehaviour
 		{
 			rb.velocity = Vector2.up * forceJump;
 			rb.transform.localScale = Vector2.one;
+			animator.SetBool(boolJump, true);
 
 			DOTween.Kill(rb.transform);
 			HandleScaleJump();
+		}
+		else
+		{
+			animator.SetBool(boolJump, false);
 		}
 	}
 
@@ -74,6 +115,5 @@ public class Player : MonoBehaviour
 	{
 		rb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
 		rb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-
 	}
 }
